30b01cec5839c6665712495ee07dcaafdad8a899
rayleigh18 <sitanggangirfan271000@gmail.com>
2020-05-06 15:07:18 +0700

    Make stringTotanggal

--- Parsing/parse.c
+++ Parsing/parse.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
-#include <parse.h>
+#include "parse.h"
 
-void 
+#define angka(c) ((int)(c)-'0')
+
+date stringToTanggal(char* data){
+    date temp;
+    temp.day = 10*angka(data[0]) + angka(data[1]);
+    temp.month = 10*angka(data[2]) + angka(data[3]);
+    temp.year = 1000*angka(data[4]) + 100*angka(data[5]) +10*angka(data[6]) + angka(data[7]);
+    return temp;
+}
+
+
+int main(){
+    char* makan = "27102000";
+    date mein = stringToTanggal(makan);
+    printf("%d %d %d", mein.day, mein.month, mein.year);
+
+    return 0;
+}
--- Parsing/parse.h
+++ Parsing/parse.h
@@ -1,6 +1,28 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+typedef struct date{
+    int day;
+    int month;
+    int year;
+}date;
+
+
+typedef struct dataPen
+{
+    int nik;
+    char* nama;
+    char* tempatLahir;
+    date tanggalLahir;
+    int umur;
+    char sex;
+    char* goldar;
+    char* status;
+    char* work;
+}dataPen;
+
+
+
 
 
 #endif
